28c809c5f924f302ad1d8ff63632e0782d400489
vitotankian <108244048+vitotankian@users.noreply.github.com>
2025-05-07 14:42:16 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -494,7 +494,7 @@ function setCustomEffects() {
 
     var timeWarping = gameData.taskData["Time Warping"]
     timeWarping.getEffect = function() {
-        var multiplier = (1 + getBaseLog(13, timeWarping.level + 1)) * 5
+        var multiplier = (1 + getBaseLog(13, timeWarping.level + 1)) * 50
         return multiplier
     }
 
